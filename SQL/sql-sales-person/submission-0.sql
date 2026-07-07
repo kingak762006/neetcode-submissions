d72cafa40585bf37fe14b49de87918cb91c22486
kingak762006 <koranneatharva7@gmail.com>
2026-07-07 21:23:45 +0530

    Add: sql-sales-person - submission-0

---
+++ SQL/sql-sales-person/submission-0.sql
@@ -0,0 +1,12 @@
+-- Write your query below
+SELECT name
+FROM sales_person
+WHERE sales_id NOT IN (
+    SELECT sales_id
+    FROM orders
+    WHERE com_id = (
+        SELECT com_id
+        FROM company
+        WHERE name = 'CRIMSON'
+    )
+);
